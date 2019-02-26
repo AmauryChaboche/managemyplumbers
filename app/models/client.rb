60f04d8e3eadb977e9a58210403da1c8d553cb20
Jeanne Bouaziz <jeannebouaziz@hotmail.fr>
2019-02-26 18:35:01 +0100

    address search added

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,5 @@
 class Client < ApplicationRecord
   has_many :bookings
-   geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  # geocoded_by :address
+  # after_validation :geocode, if: :will_save_change_to_address?
 end
