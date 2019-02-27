9b8b7330590d41d713510413c0afa006f0179fa9
Amaury Chaboche <amaury.chaboche@gmail.com>
2019-02-27 14:42:09 +0100

    fix problem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_150956) do
+ActiveRecord::Schema.define(version: 2019_02_27_120435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,8 @@ ActiveRecord::Schema.define(version: 2019_02_26_150956) do
     t.string "company"
     t.string "avatar"
     t.bigint "user_id"
+    t.string "start_hour"
+    t.string "end_hour"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["user_id"], name: "index_users_on_user_id"
