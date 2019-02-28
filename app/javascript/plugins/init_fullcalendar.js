320f5250c0e3949cc30146dc42c3c3b3b01512f3
Amaury Chaboche <amaury.chaboche@gmail.com>
2019-02-28 23:59:15 +0100

    update

--- app/javascript/plugins/init_fullcalendar.js
+++ app/javascript/plugins/init_fullcalendar.js
@@ -15,93 +15,93 @@ import swal from 'sweetalert';
 let calendar = document.getElementById('calendar');
 if (calendar) {
 
-  document.addEventListener('DOMContentLoaded', function(){
-  // $(function () {
+//   document.addEventListener('DOMContentLoaded', function(){
+//   // $(function () {
 
 
-    const updateEvent = (event) => {
-      // console.log(event);
-    }
+//     const updateEvent = (event) => {
+//       // console.log(event);
+//     }
 
-    const usersHours = JSON.parse(calendar.dataset.hours);
-    $('#calendar').fullCalendar({
-      schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
-      defaultView: 'timelineDay',
-      events: '/api/v1/bookings',
-      resources: '/api/v1/users',
-      nowIndicator: true,
-      contentHeight: 'auto',
-      slotDuration:'00:15:00',
-      minTime: '09:00:00',
-      maxTime: '19:00:00',
-      resourceLabelText: 'Plumbers',
-      eventDragStop: updateEvent ,
+//     const usersHours = JSON.parse(calendar.dataset.hours);
+//     $('#calendar').fullCalendar({
+//       schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
+//       defaultView: 'timelineDay',
+//       events: '/api/v1/bookings',
+//       resources: '/api/v1/users',
+//       nowIndicator: true,
+//       contentHeight: 'auto',
+//       slotDuration:'00:15:00',
+//       minTime: '09:00:00',
+//       maxTime: '19:00:00',
+//       resourceLabelText: 'Plumbers',
+//       eventDragStop: updateEvent ,
 
-      header: {
-      right: 'timelineDay,agendaFourDay' // buttons for switching between views
-      },
-      editable: true,
-      views: {
-      agendaFourDay: {
-        type: 'timelineDay',
-        duration: { hours: 12 },
-        buttonText: 'full day'
-      }
-    },
+//       header: {
+//       right: 'timelineDay,agendaFourDay' // buttons for switching between views
+//       },
+//       editable: true,
+//       views: {
+//       agendaFourDay: {
+//         type: 'timelineDay',
+//         duration: { hours: 12 },
+//         buttonText: 'full day'
+//       }
+//     },
 
-      eventClick: function(calEvent, jsEvent, view) {
-        console.log(calEvent);
-        // console.log(jsEvent);
-        // console.log(view);
-        const message = ' Client: ' + calEvent.client.first_name + ' ' + calEvent.client.last_name  +
-          '\n Category: ' + calEvent.intervention.category +
-          '\n Price: ' + calEvent.intervention.price +
-          '\n Duration: ' + calEvent.intervention.duration +
-          '\n Address: ' + calEvent.client.address ;
+//       eventClick: function(calEvent, jsEvent, view) {
+//         console.log(calEvent);
+//         // console.log(jsEvent);
+//         // console.log(view);
+//         const message = ' Client: ' + calEvent.client.first_name + ' ' + calEvent.client.last_name  +
+//           '\n Category: ' + calEvent.intervention.category +
+//           '\n Price: ' + calEvent.intervention.price +
+//           '\n Duration: ' + calEvent.intervention.duration +
+//           '\n Address: ' + calEvent.client.address ;
 
-        swal({
-          title: "Intervention modification?",
-          text: message,
-          buttons: {
-            cancel: "Run away!",
-            catch: {
-              text: "Throw Pokéball!",
-              value: "catch",
-            },
-            defeat: true,
-          },
-        })
-        .then((value) => {
-          switch (value) {
+//         swal({
+//           title: "Intervention modification?",
+//           text: message,
+//           buttons: {
+//             cancel: "Run away!",
+//             catch: {
+//               text: "Throw Pokéball!",
+//               value: "catch",
+//             },
+//             defeat: true,
+//           },
+//         })
+//         .then((value) => {
+//           switch (value) {
 
-            case "defeat":
-              window.location = "/clients/new";
-              break;
+//             case "defeat":
+//               window.location = "/clients/new";
+//               break;
 
-            case "catch":
-              swal("Gotcha!", "Pikachu was caught!", "success");
-              break;
+//             case "catch":
+//               swal("Gotcha!", "Pikachu was caught!", "success");
+//               break;
 
-            default:
-              swal("Got away safely!");
-          }
-        });
-      // alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
-      // alert('View: ' + view.name);
-      // // change the border color just for fun
-      // $(this).css('border-color', 'red');
+//             default:
+//               swal("Got away safely!");
+//           }
+//         });
+//       // alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
+//       // alert('View: ' + view.name);
+//       // // change the border color just for fun
+//       // $(this).css('border-color', 'red');
 
-    }
-    })
+//     }
+//     })
 
-    // const hello = document.querySelectorAll(".fc-scroller")
-    // const gg = hello[2]
-    // console.log(hello);
-    // console.log(gg);
+//     // const hello = document.querySelectorAll(".fc-scroller")
+//     // const gg = hello[2]
+//     // console.log(hello);
+//     // console.log(gg);
 
-    // [...temp1.querySelectorAll('tr')].map(line => line.innerText.split(': '));
-  })
-}
+//     // [...temp1.querySelectorAll('tr')].map(line => line.innerText.split(': '));
+//   })
+// }
 
   const eventFire = (el, etype) => {
     if (el.fireEvent) {
@@ -211,12 +211,12 @@ if (calendar) {
     eventClick: function(calEvent, jsEvent, view) {
       // console.log(calEvent);
       console.log(calEvent);
-      // console.log(view);
-              const message = ' Client: ' + calEvent.client.first_name + ' ' + calEvent.client.last_name  +
-          '\n Category: ' + calEvent.intervention.category +
-          '\n Price: ' + calEvent.intervention.price +
-          '\n Duration: ' + calEvent.intervention.duration +
-          '\n Address: ' + calEvent.client.address ;
+        // console.log(view);
+      const message = ' Client: ' + calEvent.client.first_name + ' ' + calEvent.client.last_name  +
+        '\n Category: ' + calEvent.intervention.category +
+        '\n Price: ' + calEvent.intervention.price +
+        '\n Duration: ' + calEvent.intervention.duration +
+        '\n Address: ' + calEvent.client.address ;
 
         swal({
           title: "Intervention modification?",
@@ -246,11 +246,11 @@ if (calendar) {
           }
         });
 
-  })
+  }
 
 
 })
-
+}
 
 
 
