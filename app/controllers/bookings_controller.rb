15c44b15907819223a07f98804175507b16c1377
Jeanne Bouaziz <jeannebouaziz@hotmail.fr>
2019-02-28 17:06:13 +0100

    a part of the algorithm added

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,6 @@
+require 'json'
+require 'open-uri'
+
 class BookingsController < ApplicationController
   def index
     @bookings = Booking.all
@@ -48,8 +51,28 @@ class BookingsController < ApplicationController
     # On vérfie qu'il ne finit pas après 18H #
     # On renvoie les 2 premiers avec un score entre les 2
     # Lorsqu'il choisit une des deux c'est OK -> elle a le statut VALIDEE
+  end
 
-    def calculation(timing, otherzone)
+  def calculation(booking1, booking2)
+    longitude1 = booking1.client.longitude.to_f
+    latitude1 = booking1.client.latitude.to_f
+    longitude2 = booking2.client.longitude.to_f
+    latitude2 = booking2.client.latitude.to_f
+    url = "https://api.mapbox.com/directions/v5/mapbox/driving-traffic/#{longitude1}%2C#{latitude1}%3B#{longitude2}%2C#{latitude2}.json?access_token=pk.eyJ1IjoiaGVsbG9qZWFubmUiLCJhIjoiY2pydWdjaW1qMDdpbzQzdHN0d3RmZ3NmbCJ9.WDQ36rRUNAn-4WviS51TYQ"
+    direction_serialized = open(url).read
+    direction = JSON.parse(direction_serialized)
+    return direction["routes"][0]["legs"][0]["duration"]
+  end
+
+  def find_durations(new_booking)
+    last_bookings = []
+    User.employees.all.each do |user|
+      last_bookings << user.bookings.order(:end_date).last
     end
+    durations = []
+    last_bookings.each do |booking|
+      durations << calculation(booking, new_booking)
+    end
+    p durations
   end
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,6 @@
 class Client < ApplicationRecord
   has_many :bookings, dependent: :destroy
-
-  # geocoded_by :address
-  # after_validation :geocode, if: :will_save_change_to_address?
-
+  # please don't comment these lines
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
